681d941a976c69c0faa7bb5f9a91d0d98bd9a6c8
Minh Tâm <minhtam78945@gmail.com>
2023-10-19 19:50:20 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,15 @@
+const nameGirl = "Em";
+const giftUrl = "http://nodemy.vn";
+const eventName = "Chúc Mừng 20-10";
+const titleCard = "Tặng Thư <3";
+const contentCard =
+  "Mỗi ngày anh đều cảm thấy biết ơn vì vũ trụ đã mang em đến. Mong rằng chúng ta có thể cùng đón thêm nhiều 20/10 nữa.";
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ:
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage =
+  "https://scontent.fsgn8-3.fna.fbcdn.net/v/t39.30808-6/384465897_1594115824741585_9189206462837348081_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_ohc=ZaPdabJm0PUAX_yi3mW&_nc_ht=scontent.fsgn8-3.fna&oh=00_AfDEW2U2yxoIUnrUtHTBR8N4JOhUTXpM33_uFdv3ziN7hw&oe=6535CDF2";
+const base64 = "";
+const giftImageBase64 = "data:image/png;base64, " + base64;
